8a13539b3597435eb3f7dcdf088de2132ef10c77
Alastair Brunton <info@simplyexcited.co.uk>
2009-10-14 12:32:10 +0200

    stop error out on ln -s

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -3,11 +3,16 @@ namespace :nginx do
   task :configure, :roles => :web do
     put render("nginx_vhost", binding), "#{application}.conf"
     sudo "mv #{application}.conf /etc/nginx/sites-available/#{application}.conf"
-    sudo "ln -s /etc/nginx/sites-available/#{application}.conf /etc/nginx/sites-enabled/#{application}.conf"
+    begin
+      sudo "ln -s /etc/nginx/sites-available/#{application}.conf /etc/nginx/sites-enabled/#{application}.conf"
+    rescue
+      # do nothing
+    end
+    run "rm #{application}.conf"
     stop
     start
   end
-  
+
   desc "Starts Nginx webserver"
   task :start, :roles => :web do
     sudo "/etc/init.d/nginx start"
@@ -17,5 +22,5 @@ namespace :nginx do
   task :stop, :roles => :web do
     sudo "/etc/init.d/nginx stop"
   end
-  
+
 end

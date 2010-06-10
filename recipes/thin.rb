678c8b1d06ae1e8951f37d4e256a384980fdb766
Alastair Brunton <info@simplyexcited.co.uk>
2010-06-10 21:55:59 +0200

    mods to the nginx template

--- recipes/thin.rb
+++ recipes/thin.rb
@@ -5,6 +5,13 @@ namespace :thin do
     sudo "thin config -C /etc/thin/#{application}.yml -c #{current_path}  --servers #{num_servers} -e #{rails_env} --port #{start_port}"
   end
   
+  desc "install thin"
+  task :install, :roles => :app do
+    sudo "gem install thin"
+    sudo "thin install"
+    sudo "/usr/sbin/update-rc.d -f thin defaults"
+  end
+  
   desc "start thin"
   task :start, :roles => :app do
     sudo "/etc/init.d/thin start"

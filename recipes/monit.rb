5fd52c6365d721e5345bd52de7283bac916e1ff2
Alastair Brunton <info@simplyexcited.co.uk>
2010-07-08 11:38:58 +0200

    Getting monit configuration generation working for both nginx and thin.

--- recipes/monit.rb
+++ recipes/monit.rb
@@ -2,12 +2,15 @@ namespace :monit do
   
   desc "configures monit for nginx"
   task :nginx, :roles => :web do
-    puts render('nginx_monit', binding)
+    put render('nginx_monit', binding), "nginx.conf"
+    sudo "mv nginx.conf /etc/monit/conf.d/nginx.conf"
+    sudo "/etc/init.d/monit restart"
   end
-  
   
   desc "configures monit for thin"
   task :thin, :roles => :web do
-    
+    put render('thin_monit', binding), "#{application}.conf"
+    sudo "mv #{application}.conf /etc/monit/conf.d/#{application}.conf"
+    sudo "/etc/init.d/monit restart"
   end
 end

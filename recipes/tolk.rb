eac1d4561e0954b865b4b14164609ab6cb810c91
Alastair Brunton <info@simplyexcited.co.uk>
2011-03-24 12:47:13 +0100

    adding the dump_all task for tolk as well.

--- recipes/tolk.rb
+++ recipes/tolk.rb
@@ -5,4 +5,11 @@ namespace :tolk do
     run "cd #{current_path};rake tolk:sync RAILS_ENV=#{rails_env}"
   end
   
+  desc "Runs tolk:dump_all on the remove server. Useful for when you update"
+  task :dump_all do
+    run "cd #{current_path};rake tolk:dump_all RAILS_ENV=#{rails_env}"
+  end
+  
+  
+  
 end

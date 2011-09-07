d40e67372a66bb76660f50fdb4bb724895ede338
Alastair Brunton <info@simplyexcited.co.uk>
2011-09-07 13:01:33 +0200

    changes to code

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -1,23 +1,5 @@
 namespace :apache do
-  
-  
-  desc "Rails Passenger Vhost Setup"
-  task :php_vhost do
-    
-    unless apache_port
-      set :apache_port, '80'
-    end
-    
-    logger.info "generating .conf file"
-    logger.info "placing #{application}.conf on remote server"
-    apache_conf = "/etc/apache2/sites-available/#{application}"
-    put render("php_vhost", binding), "#{application}.conf"
-    sudo "mv #{application}.conf #{apache_conf}"
-    sudo "a2ensite #{application}"
-    sudo "/etc/init.d/apache2 reload"
-  end
-  
-  
+   
   desc "Setup passenger vhost"
   task :passenger_vhost do
     logger.info "generating .conf file"
@@ -28,6 +10,24 @@ namespace :apache do
     sudo "a2ensite #{application}"
     sudo "/etc/init.d/apache2 reload"
   end
+  
+  
+  desc "PHP Vhost Setup"
+   task :php_vhost do
+
+     unless apache_port
+       set :apache_port, '80'
+     end
+
+     logger.info "generating .conf file"
+     logger.info "placing #{application}.conf on remote server"
+     apache_conf = "/etc/apache2/sites-available/#{application}"
+     put render("php_vhost", binding), "#{application}.conf"
+     sudo "mv #{application}.conf #{apache_conf}"
+     sudo "a2ensite #{application}"
+     sudo "/etc/init.d/apache2 reload"
+   end
+  
   
   
 end

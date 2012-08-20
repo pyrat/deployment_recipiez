0bfda7f8ec30862752c3ea041bd55be341b7a8da
Alastair Brunton <info@simplyexcited.co.uk>
2012-08-20 11:47:37 +0200

    making it ruby 1.8 compatible due to legacy apps using recipiez

--- recipes/foreman.rb
+++ recipes/foreman.rb
@@ -1,22 +1,22 @@
 Capistrano::Configuration.instance(true).load do
   namespace :foreman do
     desc "Export the Procfile to Ubuntu's upstart scripts"
-    task :export, roles: :app do
+    task :export, roles => :app do
       run "cd #{current_path} && sudo bundle exec foreman export upstart /etc/init -a #{application} -u #{user} -l #{shared_path}/log"
     end
 
     desc "Start the application services"
-    task :start, roles: :app do
+    task :start, roles => :app do
       sudo "service #{application} start"
     end
 
     desc "Stop the application services"
-    task :stop, roles: :app do
+    task :stop, roles => :app do
       sudo "service #{application} stop"
     end
 
     desc "Restart the application services"
-    task :restart, roles: :app do
+    task :restart, roles => :app do
       run "sudo service #{application} start || sudo service #{application} restart"
     end
     

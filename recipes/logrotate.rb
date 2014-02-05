ca1d12395573a388b1f00dc0b3207c38f6d31b88
Alastair Brunton <info@simplyexcited.co.uk>
2014-02-05 11:51:21 +0100

    Adding logrotate ec2

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -11,6 +11,19 @@ Capistrano::Configuration.instance(true).load do
       put generated, "#{application}"
       sudo "mv #{application} /etc/logrotate.d/#{application}"
     end
+
+    desc "Configure logrotate for high traffic apps on EC2"
+    task :configure_ec2_performance, :roles => [:app, :web] do
+      _cset :log_directory, 'log'
+
+      # create the directory and chown in mnt TODO
+      sudo "mkdir -p /mnt/archived_logs/#{application}"
+      sudo "chown -R  #{user}:#{user} /mnt/archived_logs"
+      generated = render('logrotate_ec2', binding)
+      puts generated
+      put generated, "#{application}"
+      sudo "mv #{application} /etc/logrotate.d/#{application}"
+    end
   end
 
 end

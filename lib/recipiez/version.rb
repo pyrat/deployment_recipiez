32313086bbba7bc8bdcfd81ffd1f39d8acb25bf7
Alastair Brunton <info@simplyexcited.co.uk>
2014-09-09 14:28:04 +0200

    adding quotes to the db pull command

--- lib/recipiez/version.rb
+++ lib/recipiez/version.rb
@@ -1,3 +1,3 @@
 module Recipiez
-  VERSION = '0.6.18'
+  VERSION = '0.6.19'
 end
--- recipes/deployment_recipiez.rb
+++ recipes/deployment_recipiez.rb
@@ -157,7 +157,7 @@ namespace :recipiez do
       filename = get_filename(application)
       cmd = "mysqldump --opt --skip-add-locks -u #{db_user} -d "
       cmd += " -h #{db_host} " if exists?('db_host')
-      cmd += " -p#{db_password} "
+      cmd += " -p'#{db_password}' "
       cmd += "#{database_to_dump} > #{archive}"
       result = run(cmd)
 

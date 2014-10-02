f778803721b78387f0dedd6596d53a69509b9669
Alastair Brunton <info@simplyexcited.co.uk>
2014-10-02 15:43:50 +0200

    Adding a move logs task to recipiez

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -18,7 +18,6 @@ Capistrano::Configuration.instance(true).load do
     task :configure_ec2_performance, :roles => [:app, :web] do
       _cset :log_directory, 'log'
 
-      # create the directory and chown in mnt TODO
       sudo "mkdir -p /mnt/archived_logs/#{application}"
       sudo "chown -R  #{user}:#{user} /mnt/archived_logs"
       generated = render('logrotate_ec2', binding)
@@ -28,6 +27,16 @@ Capistrano::Configuration.instance(true).load do
       sudo "chown root:root /etc/logrotate.d/#{application}"
       sudo "chmod 644 /etc/logrotate.d/#{application}"
     end
+
+  desc "Move log directory to /mnt"
+  task :move_log_dir_to_mnt do
+    sudo "mkdir -p /mnt/application_logs/#{application}"
+    sudo "chown -R #{user}:#{user} /mnt/application_logs/#{application}"
+    sudo "mv /var/www/apps/#{application}/shared/log/ /mnt/application_logs/#{application}"
+    sudo "ln -s /mnt/application_logs/#{application} /var/www/apps/#{application}/shared/log"
+    # Now need to deploy the app.
+  end
+
   end
 
 end

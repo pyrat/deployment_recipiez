fc60a502b468c94ec6a0330199c6801472f07080
Alastair Brunton <info@simplyexcited.co.uk>
2014-10-03 14:52:59 +0200

    release 0.6.25

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -28,20 +28,14 @@ Capistrano::Configuration.instance(true).load do
       sudo "chmod 644 /etc/logrotate.d/#{application}"
     end
 
-  desc "Move log directory to /mnt"
-  task :move_log_dir_to_mnt do
-    if Dir.exists?("/mnt/application_logs/#{application}")
-      # do nothing
-      puts "Doing nothing as the application log directory already exists in mount."
-    else
+    desc "Move log directory to /mnt"
+    task :move_log_dir_to_mnt do
       sudo "mkdir -p /mnt/application_logs/#{application}"
       sudo "chown -R #{user}:#{user} /mnt/application_logs/#{application}"
       sudo "mv /var/www/apps/#{application}/shared/log/* /mnt/application_logs/#{application}"
       sudo "rm -fr /var/www/apps/#{application}/shared/log"
       sudo "ln -s /mnt/application_logs/#{application} /var/www/apps/#{application}/shared/log"
-      # Now need to deploy the app.
     end
-  end
 
   end
 
